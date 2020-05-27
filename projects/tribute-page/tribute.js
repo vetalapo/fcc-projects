02ddc46c8a2746c49fe6a753bb90b491fcb60440
Vitaly Dushkin <vetalapo@gmail.com>
2020-05-27 17:55:37 +0300

    Tribute page: getting back home button

--- projects/tribute-page/tribute.js
+++ projects/tribute-page/tribute.js
@@ -3,3 +3,7 @@ const projectName = "tribute-page";
 document.addEventListener("DOMContentLoaded", function(event) {
     // On document ready
 });
+
+function getBackHome() {
+    window.location.href = '/';
+}
