a5f915469420adb5e764432e31bd328cd11e2862
Vitaly Dushkin <vetalapo@gmail.com>
2020-05-28 20:40:50 +0300

    Added product landing page foundation

---
+++ projects/product-landing-page/product-landing-page.js
@@ -0,0 +1,9 @@
+const projectName = "product-landing-page";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    // On Document ready
+});
+
+function getBackHome() {
+    window.location.href = "/";
+}
